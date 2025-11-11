3bf5ca683a6901c3e6523d93956c44f870d5b1b8
Asanetic <39214296+Asanetic@users.noreply.github.com>
2025-11-11 18:28:35 +0300

    Initial commit for sgps_v4 agent

--- app/symphonygps/maps/tracker/tracker.jsx
+++ app/symphonygps/maps/tracker/tracker.jsx
@@ -5,8 +5,8 @@ import { MosyNotify } from "../../../MosyUtils/ActionModals";
 import GeofenceMonitor, { computeGeofence, FloatingSearchBar, GeofenceAlerts, loadTackerProfile, loadTrackerDataCard, useGeofenceAlerts } from "../../AppCore/coreUtils";
 import { hiveRoutes } from "../../../appConfigs/hiveRoutes";
 import { MosyTitleTag } from "../../UiControl/componentControl";
-import DeviceSummaryDetails from "../../devicesummary/uiControl/DevicesummaryDetails";
 import { mosyUrlParam } from "../../../MosyUtils/hiveUtils";
+import DeviceSummaryDetails from "../../devicesummary/uiControl/DevicesummaryDetails";
 
 export default function Tracker({ devices = [] }) {
   const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
